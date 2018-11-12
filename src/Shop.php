5378d579170c0ea80aaea79f38c77ef33ee6ff35
geoff <44568889@qq.com>
2018-11-12 23:30:38 +0800

    add get shop detail

--- src/Shop.php
+++ src/Shop.php
@@ -1,20 +1,32 @@
 <?php
+
 namespace Geoff\EasyImdada;
 
 use Geoff\EasyImdada\Data\UriConfig;
 use Geoff\EasyImdada\Kernel\DataRequestClient;
 
-class Shop{
+class Shop
+{
 
-    public function add($params){
+    public function add($params)
+    {
         $uri = UriConfig::SHOP_ADD_URI;
         $dada_client = new DataRequestClient($uri, $params);
         return $dada_client->makeRequest();
     }
 
-    public function update($params){
+    public function update($params)
+    {
         $uri = UriConfig::SHOP_UPDATE_URI;
         $dada_client = new DataRequestClient($uri, $params);
+        return $dada_client->makeRequest();
+    }
+
+    public function detail($origin_shop_id)
+    {
+        $uri = '/api/shop/detail';
+        $parms = '{"origin_shop_id":"' . $origin_shop_id . '""}';
+        $dada_client = new DataRequestClient($uri, $parms);
         return $dada_client->makeRequest();
     }
 }
